7046d055e59a7416bca4c0aa04f80853ce16eeb4
soumya-ranjan-000 <8gcZU44j3RL3XQT>
2023-03-08 16:34:55 +0530

    added dynamic data to route

--- app.py
+++ app.py
@@ -1,12 +1,40 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,jsonify
 
 app = Flask(__name__)
 
+JOBS=[
+  {
+    'id':1,
+    'title': 'Data Analyst',
+    'location':'Bangaluru, India',
+    'salary':'Rs. 10,00,000'
+  },
+  {
+    'id':2,
+    'title': 'Data Engineer',
+    'location':'Pune, India',
+    'salary':'Rs. 40,00,000'
+  },
+  {
+    'id':3,
+    'title': 'Front-End Developer',
+    'location':'Kochi, India'
+  },
+  {
+    'id':4,
+    'title': 'Back-End Developer',
+    'location':'BBSR, India',
+    'salary':'Rs. 19,00,000'
+  }
+]
 
 @app.route("/")
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html',jobs=JOBS,company_name='Wipro')
 
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', debug=True)
+  app.run(port='8080', debug=True)
